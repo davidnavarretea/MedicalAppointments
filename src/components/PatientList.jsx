7b2b2eb1053cada3e8c48ea9e38a8651861c7def
David <davidnavarretea1996@gmail.com>
2022-05-29 09:22:44 +0200

    media querie scroll pacient list

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,7 +1,7 @@
 import Patient from "./Patient"
 const PatientList = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
     <h2 className="font-black text-3xl text-center">Patient List</h2>
     <p className="text-lg mt-5 text-center mb-10">
       Manage your {''}
