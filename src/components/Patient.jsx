c8c30668ab01a908b9be4783945fc820e42cd366
David <davidnavarretea1996@gmail.com>
2022-06-07 13:47:46 +0200

    Interaction added

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -1,21 +1,21 @@
-const Patient = () => {
+const Patient = ({patient}) => {
   return (
     <div className="bg-white my-3 mx-5 px-5 py-10 shadow-md rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Name: {''}
-        <span className="font-normal normal-case">David</span>
+        <span className="font-normal normal-case">{patient.name}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Email: {''}
-        <span className="font-normal normal-case">david@david.com</span>
+        <span className="font-normal normal-case">{patient.email}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Patient Discharge: {''}
-        <span className="font-normal normal-case">1 June 2022</span>
+        <span className="font-normal normal-case">{patient.discharge}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Symptoms: {''}
-        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi perspiciatis minus beatae quas nobis deserunt omnis, quae recusandae debitis fugiat repellendus harum tempora modi commodi veniam sequi id possimus cum.</span>
+        <span className="font-normal normal-case">{patient.symptoms}</span>
       </p>
     </div>
   )
--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -2,18 +2,25 @@ import Patient from "./Patient"
 const PatientList = ({patients}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
-    <h2 className="font-black text-3xl text-center">Patient {''} <span className="text-blue-600">List</span></h2>
-    <p className="text-lg mt-5 text-center mb-10 font-bold">
-      Manage your {''}
-      <span className="text-blue-600 font-bold">Patients and Appointments</span>
-    </p>
-    <Patient/>
-    <Patient/>
-    <Patient/>
-    <Patient/>
-    <Patient/>
-    <Patient/>
-    <Patient/>
+      {/* If patients not empty */}
+      {patients.length !== 0 ?
+        <>
+          <h2 className="font-black text-3xl text-center">Patient {''} <span className="text-blue-600">List</span></h2>
+          <p className="text-lg mt-5 text-center mb-10 font-bold">
+          Manage your {''}
+            <span className="text-blue-600 font-bold">Patients and Appointments</span>
+          </p>
+          {patients.map(patient => (<Patient patient={patient} key={patient.id}/>))}
+        </> :
+        // If patients empty
+        <>
+          <h2 className="font-black text-3xl text-center">Empty Patient {''} <span className="text-blue-600">List</span></h2>
+          <p className="text-lg mt-5 text-center mb-10 font-bold">
+          Create your {''}
+            <span className="text-blue-600 font-bold">Patients and Appointments</span>
+          </p>
+        </>
+      }
     </div>
   )
 }
